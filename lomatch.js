9b126d04c8e2b7ee5193c1bc402e094dd905b60c
Tim Mendez <tim.mendez@originate.com>
2018-05-18 11:28:07 -0700

    type -> inputType

--- lomatch.js
+++ lomatch.js
@@ -25,7 +25,7 @@ function match(input, output, predicates, funcs) {
     }
 
     // Take all the iteratee preddies and return them, let the user decide which preddies to use in their thing
-    if (func.type === 'iteratee') {
+    if (func.inputType === 'iteratee') {
       var iteratees = test.testWithIteratees(input, output, func);
       if (!_.isEmpty(iteratees)) {
         matches.push({func: func.name, iteratees: iteratees});
