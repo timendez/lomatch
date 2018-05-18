96023b62ec4826e61726fd6d79833567e52c1890
Tim Mendez <tim.mendez@originate.com>
2018-05-18 11:37:39 -0700

    fix rename, remove extra testing variable

--- test/iteratees.js
+++ test/iteratees.js
@@ -7,16 +7,14 @@ describe('Iteratees as predicates', () => {
     describe('_.uniqBy', () => {
       it('with floor', () => {
         var matches = lomatch.LoMatch([2.1, 1.2, 2.3], [2.1, 1.2], []);
-        var expectedMatch = '_.floor';
         var targetMatch = _.find(matches, ['func', '_.uniqBy']);
         expect(targetMatch).to.have.a.property('iteratees');
-        expect(targetMatch.iteratees).to.include(expectedMatch);
+        expect(targetMatch.iteratees).to.include('_.floor');
       });
     });
     describe('_.unzipWith', () => {
       it('with add', () => {
         var matches = lomatch.LoMatch([[1, 10, 100], [2, 20, 200]], [3, 30, 300], []);
-        var expectedMatch = '_.floor';
         var targetMatch = _.find(matches, ['func', '_.unzipWith']);
         expect(targetMatch).to.have.a.property('iteratees');
         expect(targetMatch.iteratees).to.include('_.add');
@@ -25,7 +23,6 @@ describe('Iteratees as predicates', () => {
     describe('_.sortedUniqBy', () => {
       it('with floor', () => {
         var matches = lomatch.LoMatch([1.1, 1.2, 2.3, 2.4], [1.1, 2.3], []);
-        var expectedMatch = '_.floor';
         var targetMatch = _.find(matches, ['func', '_.sortedUniqBy']);
         expect(targetMatch).to.have.a.property('iteratees');
         expect(targetMatch.iteratees).to.include('_.floor');
@@ -37,46 +34,41 @@ describe('Iteratees as predicates', () => {
     describe('_.invertBy', () => {
       it('with floor', () => {
         var matches = lomatch.LoMatch({ 'a': '1.2', 'b': '2.2', 'c': '1.7' }, { 1: [ 'a', 'c' ], 2: [ 'b' ] }, []);
-        var expectedMatch = '_.floor';
         var targetMatch = _.find(matches, ['func', '_.invertBy']);
         expect(targetMatch).to.have.a.property('iteratees');
-        expect(targetMatch.iteratees).to.include(expectedMatch);
+        expect(targetMatch.iteratees).to.include('_.floor');
       });
     });
     describe('_.mapKeys', () => {
       it('with sum', () => {
         var matches = lomatch.LoMatch({ 1: 2, 3: 4 }, { 0: 4 }, []);
-        var expectedMatch = '_.sum';
         var targetMatch = _.find(matches, ['func', '_.mapKeys']);
         expect(targetMatch).to.have.a.property('iteratees');
-        expect(targetMatch.iteratees).to.include(expectedMatch);
+        expect(targetMatch.iteratees).to.include('_.sum');
       });
     });
     describe('_.mapValues', () => {
       it('with add', () => {
         var matches = lomatch.LoMatch({ key: ' value' }, { key: ' valuekey' }, []);
-        var expectedMatch = '_.add';
         var targetMatch = _.find(matches, ['func', '_.mapValues']);
         expect(targetMatch).to.have.a.property('iteratees');
-        expect(targetMatch.iteratees).to.include(expectedMatch);
+        expect(targetMatch.iteratees).to.include('_.add');
       });
     });
     describe('_.omitBy', () => {
       it('with isNumber', () => {
         var matches = lomatch.LoMatch({ str: 'hello', num1: 3, num2: 5 }, { str: 'hello' }, []);
-        var expectedMatch = '_.isNumber';
         var targetMatch = _.find(matches, ['func', '_.omitBy']);
         expect(targetMatch).to.have.a.property('iteratees');
-        expect(targetMatch.iteratees).to.include(expectedMatch);
+        expect(targetMatch.iteratees).to.include('_.isNumber');
       });
     });
     describe('_.pickBy', () => {
       it('with isNumber', () => {
         var matches = lomatch.LoMatch({ str: 'hello', num1: 3, num2: 5 }, { num1: 3, num2: 5 }, []);
-        var expectedMatch = '_.isNumber';
         var targetMatch = _.find(matches, ['func', '_.pickBy']);
         expect(targetMatch).to.have.a.property('iteratees');
-        expect(targetMatch.iteratees).to.include(expectedMatch);
+        expect(targetMatch.iteratees).to.include('_.isNumber');
       });
     });
   });
